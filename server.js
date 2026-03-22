8b11fb4f95ec48f81115809e191b6d45401b01fe
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-03-22 07:46:26 +0100

    Changed port

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
   res.send("SmartInventoryAI API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
