91096c31d14b0eb01b6d243a14183383d7d093fa
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-11 18:41:39 +0200

    feat(suppliers): add protected supplier creation endpoint

--- routes/suppliersRoute.js
+++ routes/suppliersRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getSuppliers } from "../controllers/suppliersController.js";
+import {
+  addSupplier,
+  getSuppliers,
+} from "../controllers/suppliersController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 
 const router = express.Router();
 
 router.get("/", verifyJWT, getSuppliers);
+router.post("/", verifyJWT, addSupplier);
 export default router;
