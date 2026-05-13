e369b17541d9f8cdf84b70b9f506f2c3b20cfb32
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-13 17:46:15 +0200

    add item creation with validation and upsert logic

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,4 +1,6 @@
 import { db } from "../db.js";
+import { validateItem } from "../utils/validators/itemValidator.js";
+import { normalizeItem } from "../utils/validators/normalizeItem.js";
 
 /**
  * get all itmes
@@ -54,5 +56,81 @@ export const getAllItems = async (req, res) => {
     return res.status(500).json({
       error: "Failed to fetch items",
     });
+  }
+};
+
+/**
+ * add item
+ */
+export const addItem = async (req, res) => {
+  const userId = Number(req.user.id);
+
+  if (!Number.isInteger(userId) || userId <= 0) {
+    return res.status(400).json({
+      error: "user_id required",
+    });
+  }
+  const payload = normalizeItem(req.body);
+  const errors = validateItem(payload);
+  if (errors.length > 0) return res.status(400).json({ errors });
+
+  try {
+    const result = await db.query(
+      `
+        INSERT INTO items
+          (user_id, category_id, name, quantity, min_quantity, supplier_id, price, description)
+        VALUES
+          ($1, $2, $3, $4, $5, $6, $7, $8)
+        ON CONFLICT (name, supplier_id, user_id)
+        DO UPDATE SET quantity = items.quantity + EXCLUDED.quantity
+        RETURNING *, (xmax = 0) AS created;
+      `,
+      [
+        userId,
+        payload.category_id,
+        payload.name,
+        payload.quantity,
+        payload.min_quantity,
+        payload.supplier_id,
+        payload.price,
+        payload.description,
+      ],
+    );
+    const item = result.rows[0];
+
+    // Pobierz pełne dane z JOIN
+    const fullData = await db.query(
+      `
+      SELECT 
+        i.*,
+        s.name AS supplier_name,
+        c.name AS category_name
+      FROM items i
+      LEFT JOIN suppliers s ON s.id = i.supplier_id
+      LEFT JOIN categories c ON c.id = i.category_id
+      WHERE i.id = $1
+      `,
+      [item.id],
+    );
+    const fullItem = fullData.rows[0];
+
+    if (item.created) {
+      return res.status(201).json({
+        id: fullItem.id,
+        item: fullItem,
+        created: true,
+        updated: false,
+      });
+    } else {
+      return res.status(200).json({
+        id: fullItem.id,
+        item: fullItem,
+        created: false,
+        updated: true,
+      });
+    }
+  } catch (error) {
+    console.error("addItem error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllItems } from "../controllers/itemsController.js";
+import { getAllItems, addItem } from "../controllers/itemsController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 
 const router = express.Router();
 
 router.get("/", verifyJWT, getAllItems);
-
+router.post("/", verifyJWT, addItem);
 export default router;
