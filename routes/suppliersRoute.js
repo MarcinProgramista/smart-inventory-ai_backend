6510668331cffa2b64370b3689fccc429e7d0897
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-05-30 17:39:52 +0200

    feat: add advanced supplier search route

--- routes/suppliersRoute.js
+++ routes/suppliersRoute.js
@@ -4,12 +4,14 @@ import {
   getSuppliers,
   updateSupplier,
   deleteSupplier,
+  searchSuppliersAdvanced,
 } from "../controllers/suppliersController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 
 const router = express.Router();
 
 router.get("/", verifyJWT, getSuppliers);
+router.get("/search", verifyJWT, searchSuppliersAdvanced);
 router.post("/", verifyJWT, addSupplier);
 router.patch("/:id", verifyJWT, updateSupplier);
 router.delete("/:id", verifyJWT, deleteSupplier);
